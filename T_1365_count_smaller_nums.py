a863e672323d4a9cd1bacf61d11b52713e3bd75e
cypher1992 <ryassin92@gmail.com>
2024-02-20 16:09:17 -0500

    test commit

--- T_1365_count_smaller_nums.py
+++ T_1365_count_smaller_nums.py
@@ -22,24 +22,27 @@ class Solution:
 
 class MyTestCase(unittest.TestCase):
     case_1 = [8,1,2,2,3]
-    case_2 = [["B","C"],["D","B"],["C","A"]]
-    case_3 = [["A","Z"]]
+    output1 = [4,0,1,1,3]
+    case_2 = [6,5,4,8]
+    output2 = [2,1,0,3]
+    case_3 = [7,7,7,7]
+    output3 = [0,0,0,0]
     function = Solution.smallerNumbersThanCurrent
 
 
     def test_case1(self):
         output = self.function(self.case_1)
-        self.assertEqual([4,0,1,1,3], output)  # add assertion here
+        self.assertEqual(self.output1, output)  # add assertion here
 
     def test_case2(self):
         output = self.function(self.case_2)
         print(output)
-        self.assertEqual("A", output)  # add assertion here
+        self.assertEqual(self.output2, output)  # add assertion here
 
     def test_case3(self):
         output = self.function(self.case_3)
         print(output)
-        self.assertEqual("Z", output)  # add assertion here
+        self.assertEqual(self.output3, output)  # add assertion here
 
 if __name__ == '__main__':
     unittest.main()
