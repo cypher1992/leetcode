d083871799d7d7579fa0a4467f761b1df8f451c1
cypher1992 <ryassin92@gmail.com>
2024-02-20 14:30:36 -0500

    test commit

---
+++ reverse_string_344.py
@@ -0,0 +1,25 @@
+import unittest
+
+
+class Solution:
+    def reverseString(self, s: list) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        https://leetcode.com/problems/reverse-string/
+        """
+        return [s[i-1] for i in range(len(s), 0,-1)]
+
+
+class MyTestCase(unittest.TestCase):
+
+    def test_case1(self):
+        output = Solution().reverseString(["h","e","l","l","o"])
+        self.assertEqual(["o","l","l","e","h"], output)  # add assertion here
+
+
+    def test_case2(self):
+        output = Solution().reverseString(["H","a","n","n","a","h"])
+        self.assertEqual(["h","a","n","n","a","H"], output)  # add assertion here
+
+if __name__ == '__main__':
+    unittest.main()
