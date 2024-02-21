5c85798db89101e3364909b0f2e2f5181c9cb09b
cypher1992 <ryassin92@gmail.com>
2024-02-21 12:14:51 -0500

    test commit

---
+++ BacktoBasics/algo_n_ds/linkedlist/underthehood.py
@@ -0,0 +1,18 @@
+
+
+if __name__ == '__main__':
+    head = {
+        "value": 11,
+        "next": {
+            "value": 3,
+            "next": {
+                "value": 23,
+                "next": {
+                    "value": 7,
+                    "next": None
+                }
+            }
+        }
+    }
+
+    print(head["next"]["next"]["value"] == 23)
