e161f3dc3a8dbab8df5eb16c71057db936ddc335
cypher1992 <ryassin92@gmail.com>
2024-02-21 12:49:17 -0500

    test commit

--- BacktoBasics/algo_n_ds/linkedlist/linkedlist.py
+++ BacktoBasics/algo_n_ds/linkedlist/linkedlist.py
@@ -6,17 +6,47 @@ class Node:
 
 class LinkedList:
 
-    def __init__(self,value):
+    def __init__(self,value=None):
         new_node = Node(value)
         self.head = new_node
         self.tail = new_node
         self.length = 1
 
     def append(self,value):
-        pass
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length +=1
+        return True
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        pre = self.head
+        while(temp.next):
+            pre = temp
+            temp = temp.next
+        self.tail = pre
+        self.tail.next = None
+        self.length -= 1
+        if self.length == 0:
+            self.head = None
+            self.tail = None
+        return temp
 
     def prepend(self, value):
         pass
 
     def insert(self, value):
         pass
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp=temp.next
--- BacktoBasics/algo_n_ds/linkedlist/linkedlist_test.py
+++ BacktoBasics/algo_n_ds/linkedlist/linkedlist_test.py
@@ -3,10 +3,34 @@ from BacktoBasics.algo_n_ds.linkedlist.linkedlist import LinkedList
 
 class MyTestCase(unittest.TestCase):
 
-    def test_something(self):
+    def test_head_is_None(self):
+        myll = LinkedList()
+        self.assertEqual(myll.head.value, None)
+
+    def test_tail_is_None(self):
+        myll = LinkedList()
+        self.assertEqual(myll.tail.value, None)
+
+    def test_length_is_None(self):
+        myll = LinkedList()
+        self.assertEqual(myll.length, 1)
+
+    def test_head(self):
         myll = LinkedList(4)
         self.assertEqual(myll.head.value, 4)  # add assertion here
 
+    def test_tail(self):
+        myll = LinkedList(4)
+        self.assertEqual(myll.tail.value, 4)
+
+    def test_length(self):
+        myll = LinkedList(4)
+        self.assertEqual(myll.length, 1)
+
+    def test_append(self):
+        myll = LinkedList(1)
+        myll.append(2)
+        myll.print_list()
 
 if __name__ == '__main__':
     unittest.main()
