4b322297a21a18ad67a35421760d3c6f88a16fde
cypher1992 <ryassin92@gmail.com>
2024-02-22 15:24:35 -0500

    test commit

---
+++ leetcode_tests/T_14_longest_common_prefix.py
@@ -0,0 +1,47 @@
+import unittest
+from typing import List
+
+class Solution:
+    """
+        https://leetcode.com/problems/longest-common-prefix/description/
+    """
+
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        cpf = ""
+        sortedStrs = sorted(strs)
+        shortestStr = sortedStrs[0]
+        longestStr = sortedStrs[-1]
+        for char1, char2 in zip(shortestStr, longestStr):
+            if char1 != char2:
+                break
+            cpf += char1
+        return cpf
+
+
+class SolutionTestCase(unittest.TestCase):
+    case_1 = ["flower","flow","flight"]
+    output1 = "fl"
+    case_2 = ["dog","racecar","car"]
+    output2 = ""
+    function = Solution.longestCommonPrefix
+
+    def test_case1(self):
+        output = self.function(self.case_1,)
+        self.assertEqual(self.output1, output)  
+
+    def test_case2(self):
+        output = self.function(self.case_2)
+        print(output)
+        self.assertEqual(self.output2, output)
+
+
+    def test_zip(self):
+        string = "Hello"
+        string2 = "Hello2"
+        for char1, char2 in zip(string,string2):
+            print("String One Char", char1)
+            print("String Two Char", char2)
+
+
+if __name__ == '__main__':
+    unittest.main()
