c123192e9e4a1dc9320e866dea7f6a3822cb19de
cypher1992 <ryassin92@gmail.com>
2024-02-21 19:51:48 -0500

    test commit

--- BacktoBasics/algo_n_ds/linkedlist/linkedlist_test.py
+++ BacktoBasics/algo_n_ds/linkedlist/linkedlist_test.py
@@ -32,5 +32,50 @@ class MyTestCase(unittest.TestCase):
         myll.append(2)
         myll.print_list()
 
+    def test_pop(self):
+        myll = LinkedList(1)
+        myll.append(2)
+        print(myll.pop())
+        print(myll.pop())
+        print(myll.pop())
+
+    def test_something(self):
+        def word_to_number(word):
+            word = word.lower()
+            word_dict = {
+                "zero": "0", "one": "1", "two": "2", "three": "3", "four": "4",
+                "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9",
+                "ten": "10", "eleven": "11", "twelve": "12", "thirteen": "13",
+                "fourteen": "14", "fifteen": "15", "sixteen": "16", "seventeen": "17",
+                "eighteen": "18", "nineteen": "19", "twenty": "20", "thirty": "30",
+                "forty": "40", "fifty": "50", "sixty": "60", "seventy": "70",
+                "eighty": "80", "ninety": "90", "hundred": "100", "thousand": "1000",
+                "million": "1000000", "billion": "1000000000"
+            }
+
+            if word in word_dict:
+                return word_dict[word]
+
+            # Handle composite numbers like "twenty-one", "thirty-two", etc.
+            if '-' in word:
+                prefix, suffix = word.split('-')
+                return str(int(word_dict[prefix]) + int(word_dict[suffix]))
+
+            return "Not a recognized number word"
+
+        def text_to_number(text):
+            words = text.split()
+            numeric_words = []
+            for word in words:
+                if word.lower() == 'and':
+                    continue
+                numeric_word = word_to_number(word)
+                if numeric_word != "Not a recognized number word":
+                    numeric_words.append(numeric_word)
+            return str(sum(map(int, numeric_words)))
+
+
+        print(text_to_number("one billion two hundred thirty eight million"))
+
 if __name__ == '__main__':
     unittest.main()
