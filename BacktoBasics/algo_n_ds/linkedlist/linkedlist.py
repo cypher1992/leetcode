4be69aa82a53ff8cbda56f209bd9837a980d8e4c
cypher1992 <ryassin92@gmail.com>
2024-02-25 20:55:29 -0500

    test commit

--- BacktoBasics/algo_n_ds/linkedlist/linkedlist.py
+++ BacktoBasics/algo_n_ds/linkedlist/linkedlist.py
@@ -39,3 +39,16 @@ class LinkedList:
             self.tail = None
         return temp
 
+    def prepend(self, value):
+        # add new item to the beginning of the list
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length +=1
+        return True
+
