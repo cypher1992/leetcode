ed02565b96fa4eefcef9527934f3939455e686f5
cypher1992 <ryassin92@gmail.com>
2024-02-25 20:55:59 -0500

    test commit

--- BacktoBasics/algo_n_ds/linkedlist/linkedlist.py
+++ BacktoBasics/algo_n_ds/linkedlist/linkedlist.py
@@ -79,3 +79,8 @@ class LinkedList:
             return True
         return False
 
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp=temp.next
