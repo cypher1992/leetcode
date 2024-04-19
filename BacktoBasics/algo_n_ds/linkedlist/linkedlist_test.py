a01fc66f88f6a43ae8cc303ef893284955485c94
cypher1992 <ryassin92@gmail.com>
2024-04-19 02:07:24 -0400

    Python Basics

--- BacktoBasics/algo_n_ds/linkedlist/linkedlist_test.py
+++ BacktoBasics/algo_n_ds/linkedlist/linkedlist_test.py
@@ -372,5 +372,41 @@ class MyTestCase(unittest.TestCase):
             return factorial(n-1)*n
         self.assertEqual(factorial(5),120)
 
+    def test_hinao(self):
+        class Stack:
+            def __init__(self):
+                self.__index = []
+
+            def __len__(self):
+                return len(self.__index)
+
+            def push(self,item):
+                self.__index.insert(0,item)
+
+            def peek(self):
+                if len(self) == 0:
+                    raise Exception("peek() called on empty stack")
+                return self.__index[0]
+
+            def pop(self):
+                if len(self)==0:
+                    raise Exception("pop() called on empty stack")
+                return self.__index.pop(0)
+
+            def __str__(self):
+                return str(self.__index)
+
+
+        s1 = Stack()
+        s2 = Stack()
+        s3 = Stack()
+
+        for i in range(3,0,-1):
+            s3.push(i)
+        print(str(s3))
+
+        s2.push(s3.pop())
+        print(str(s3),str(s2))
+
 if __name__ == '__main__':
     unittest.main()
